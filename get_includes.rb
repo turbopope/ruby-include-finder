07d327e0372fb6a16fa207e586e85dd5b3e16e37
turbopope <mbrack@uni-koblenz.de>
2016-12-13 12:18:31 +0100

    Externalize get_includes

---
+++ get_includes.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+require 'json'
+
+
+# Searches filename for all includes/requires and resolves them to absolute paths.
+# Resolutions are logged with depth*4 spaces indentation and are added to resolved.
+# Then recursively calls this method with more depth and merges these resolutions int resolved before returning it.
+INCLUDE_PATTERN = /[^#]*(?<type>(require|include)(_relative)?)\s+\(?["'](?<inc>.*?)["']/
+def get_includes(gemfile, filename, depth=0, resolved=Hash.new)
+  return Hash.new unless File.extname(filename) == ".rb"
+  File.open(filename) do |file|
+    includes = file.grep(INCLUDE_PATTERN)
+    includes.each do |inc|
+      match = INCLUDE_PATTERN.match(inc)
+      type = match['type'].strip.downcase
+      inc = match['inc'].strip.downcase
+      next if resolved.has_key?(inc)
+      # puts "    -  #{type} #{inc}"
+      if type == 'require'
+        source_file = `ruby search #{gemfile} #{inc}`.strip
+        puts "#{(' ') * (depth * 4)}- #{inc}: #{source_file}" unless depth == 0
+        resolved.store(inc, source_file)
+        resolved.merge(get_includes(source_file, gemfile, depth == 0 ? 0 : depth+1, resolved))
+      end
+    end
+  end
+  return resolved
+end
+
+if __FILE__ == $0 # Do not run when being included
+  puts JSON.pretty_generate(get_includes(ARGV[0], ARGV[1], 0).select{|k, v| v != ''})
+end
