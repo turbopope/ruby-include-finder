627744d56c71bd5c519368212454b1e8024f110e
turbopope <mbrack@uni-koblenz.de>
2016-12-13 17:57:09 +0100

    Externalize get_method_calls

---
+++ get_method_calls.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'parser/current'
+require 'json'
+require_relative('send_processor')
+
+def get_method_calls(filename)
+  ast = Parser::CurrentRuby.parse(File.read(filename))
+  send_processor = SendProcessor.new
+  send_processor.process(ast)
+  send_processor.method_calls
+end
+
+if __FILE__ == $0
+  puts JSON.pretty_generate(get_method_calls(ARGV[0]).to_a)
+end
